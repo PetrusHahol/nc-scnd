9e689b3757c8fec7ea6fde012f454c79a634c948
PetrusHahol <petius96@gmail.com>
2016-11-28 16:46:51 +0300

    after blue screen

--- My_project2/src/com/netcracker/petrusev/project2/command/ActionCommand.java
+++
@@ -1,7 +0,0 @@
-package com.netcracker.petrusev.project2.command;
-import javax.servlet.http.HttpServletRequest;
-public interface ActionCommand {
-
-String execute(HttpServletRequest request);
-
-}
